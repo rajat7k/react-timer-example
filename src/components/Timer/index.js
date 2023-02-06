a0a9eedc414a0adef7b70e712a85d0e87486e4f3
Rajat <kaushikrajat777.com>
2023-02-06 14:50:57 +0530

    .

---
+++ src/components/Timer/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react'
+import './index.css'
+
+export default function Timer() {
+  const [curTime, setCurTime] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect")
+    const startTimer=setInterval(() => {
+      setCurTime(prev=>prev+1);
+    }, 1000)
+    return()=> clearInterval(startTimer);
+  }, [])
+  return (
+    <div className="timer-page">
+      <p className="timer-heading">
+        Timer
+      </p>
+      <p className="timer-value">
+        {curTime}
+      </p>
+    </div>
+  )
+}
